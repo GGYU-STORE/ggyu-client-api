f608d141cf550202adb7753242230b88bb7d55b5
changha <chjeong@leevi.co.kr>
2023-01-21 15:45:50 +0900

    init project

---
+++ src/main/java/com/ggyu/client/GgyuClientApplication.java
@@ -0,0 +1,13 @@
+package com.ggyu.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgyuClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgyuClientApplication.class, args);
+	}
+
+}
