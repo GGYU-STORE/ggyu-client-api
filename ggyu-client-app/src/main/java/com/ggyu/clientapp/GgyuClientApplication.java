2c4091cbd17734ca44104afaa22b7fdc4618fd32
changha <chjeong@leevi.co.kr>
2023-02-20 00:22:43 +0900

    - datasource 추가 - base 엔티티 스캔 설정

--- ggyu-client-app/src/main/java/com/ggyu/clientapp/GgyuClientApplication.java
+++ ggyu-client-app/src/main/java/com/ggyu/clientapp/GgyuClientApplication.java
@@ -2,8 +2,10 @@ package com.ggyu.clientapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@EntityScan("com.ggyu.base")
+@SpringBootApplication(scanBasePackages = "com.ggyu.base")
 public class GgyuClientApplication {
 
 	public static void main(String[] args) {
